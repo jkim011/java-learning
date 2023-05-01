0e61f31ccdf6a5b31bff7530e5fee2cd7e74ac67
Jared Kim <jaredkim01@yahoo.com>
2023-05-01 16:32:17 -0500

    for loops, switches, logical ops

--- src/Main.java
+++ src/Main.java
@@ -112,6 +112,72 @@ public class Main {
 //
 //        System.out.println(integerRandom);
 
+    ////////////////////////////////////////////// IF STATEMENTS //////////////////////////////////////////////////////
+//        int age = 12;
+//
+//        if(age >= 75){
+//            System.out.println("You old!");
+//        } else if(age >= 18) {
+//            System.out.println("You are an adult!");
+//        } else if(age >= 13) {
+//            System.out.println("You are a teenager!");
+//        } else {
+//            System.out.println("You are a child!");
+//        }
+
+    /////////////////////////////////////////////// SWITCHES //////////////////////////////////////////////////////////
+        // switch = statement that allows a variable to be tested for equality against a list of values. better than a long string of if statements
+
+//        String day = "pizza";
+//
+//        switch(day) {
+//            case "Sunday": System.out.println("It is Sunday!");
+//            break;
+//            case "Monday": System.out.println("It is Monday!");
+//            break;
+//            case "Tuesday": System.out.println("It is Tuesday!");
+//            break;
+//            case "Wednesday": System.out.println("It is Wednesday!");
+//            break;
+//            case "Thursday": System.out.println("It is Thursday!");
+//            break;
+//            case "Friday": System.out.println("It is Friday!");
+//            break;
+//            case "Saturday": System.out.println("It is Saturday!");
+//            break;
+//            default: System.out.println("That is not a day!");
+//        }
+
+    /////////////////////////////////// LOGICAL OPERATORS /////////////////////////////////////////////////////////////
+        // logical operators = used to connect two or more expressions
+        // && = (AND) both conditions must be true
+        // || = (OR) either condition must be true
+        // ! = (NOT) reverses boolean value of condition
+
+//        int temp = 25;
+//
+//        if(temp > 30) {
+//            System.out.println("It is hot outside");
+//        } else if(temp >= 20 && temp <= 30) {
+//            System.out.println("It is warm outside");
+//        } else {
+//            System.out.println("It is cold outside");
+//        }
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("You are playing a game! Press q or Q to quit");
+//        String response = scanner.next();
+//
+//        if(response.equals("q") || response.equals("Q")) {
+//            System.out.println("You quit the game");
+//        } else {
+//            System.out.println("You are still playing the game");
+//        }
+
+    /////////////////////////////////// WHILE LOOP ////////////////////////////////////////////////////////////////////
+        // while loop = executes a block of code as long as it's condition remains true
+
 
 
     }
