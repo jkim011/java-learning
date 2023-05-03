d2e1bac7984071c8a2b3f559d4966c4d95337d0c
Jared Kim <jaredkim01@yahoo.com>
2023-05-02 22:34:30 -0500

    while loops

--- src/Main.java
+++ src/Main.java
@@ -177,8 +177,24 @@ public class Main {
 //        }
 
     /////////////////////////////////// WHILE LOOP ////////////////////////////////////////////////////////////////////
-        // while loop = executes a block of code as long as it's condition remains true
+//        // while loop = executes a block of code as long as it's condition remains true
+//
+//        Scanner scanner = new Scanner(System.in);
+//        String name = "";
+//
+////        while(name.isBlank()) {  // while loop
+////            System.out.print("Enter your name: ");
+////            name = scanner.nextLine();
+////        }
+//
+//        do {  // do loop variation. always performs block of code at least once, and THEN check the while condition after
+//            System.out.print("Enter your name: ");
+//            name = scanner.nextLine();
+//        } while(name.isBlank());
+//
+//        System.out.println("Hello " + name);
 
+    ///////////////////////////// FOR LOOPS ////////////////////////////////////////////////////////////////////////////
 
 
     }
