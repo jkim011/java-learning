8d70ff0ca541cb5299b6272092ee5b351f8bf6a6
Jared Kim <jaredkim01@yahoo.com>
2023-05-03 14:41:16 -0500

    overloaded constructors

---
+++ src/overloadedConstructors/ovCons.java
@@ -0,0 +1,28 @@
+//package overloadedConstructors;
+//
+//public class ovCons {
+//    public static void main(String[] args) {
+//        // overloaded constructors allow us to make variations of our constructors without changing the class parameters
+//
+//        Pizza pepperoniPizza = new Pizza("thick crust", "tomato", "white", "pepperoni");
+//        System.out.println("Here are the ingredients of your sausage pizza: ");
+//        System.out.println(pepperoniPizza.bread);
+//        System.out.println(pepperoniPizza.sauce);
+//        System.out.println(pepperoniPizza.cheese);
+//        System.out.println(pepperoniPizza.topping);
+//        System.out.println();
+//
+//        Pizza cheesePizza = new Pizza("thick crust", "tomato", "white");
+//        System.out.println("Here are the ingredients of your cheese pizza: ");
+//        System.out.println(cheesePizza.bread);
+//        System.out.println(cheesePizza.sauce);
+//        System.out.println(cheesePizza.cheese);
+//        System.out.println();
+//
+//        Pizza veganPizza = new Pizza("thick crust", "tomato");
+//        System.out.println("Here are the ingredients of your vegan pizza: ");
+//        System.out.println(veganPizza.bread);
+//        System.out.println(veganPizza.sauce);
+//
+//    }
+//}
