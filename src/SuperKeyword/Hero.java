a3161f21788225c225e9fc7c61ab33c0118e5bdb
Jared Kim <jaredkim01@yahoo.com>
2023-05-04 11:31:59 -0500

    super keyword

---
+++ src/SuperKeyword/Hero.java
@@ -0,0 +1,14 @@
+//package SuperKeyword;
+//
+//public class Hero extends Person {
+//    String power;
+//
+//    Hero(String name, int age, String power) {
+//        super(name, age); // referring to whats in parent class Person
+//        this.power = power;
+//    }
+//
+//    public String toString() {
+//        return super.toString() + this.power; // turns our parent class Person to string and adds power attribute from this class
+//    }
+//}
