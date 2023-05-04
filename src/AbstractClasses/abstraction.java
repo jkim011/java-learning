6b63cd28c8ff655a869c879e18837a02acafa740
Jared Kim <jaredkim01@yahoo.com>
2023-05-04 11:41:53 -0500

    abstract classes

---
+++ src/AbstractClasses/abstraction.java
@@ -0,0 +1,14 @@
+//package AbstractClasses;
+//
+//public class abstraction {
+//    public static void main(String[] args) {
+//        // abstract = abstract classes cannot be instantiated, but they can have a subclass
+//        //            abstract methods are declared without an implementation
+//
+//        // Vehicle vehicle = new Vehicle(); // calling vehicle won't work cuz we made Vehicle.java abstract
+//        Car car = new Car();
+//
+//        car.go();
+//
+//    }
+//}
