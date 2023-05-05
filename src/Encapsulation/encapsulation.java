3001f65ee0c3a3101b33caeb008f0ff148e89749
Jared Kim <jaredkim01@yahoo.com>
2023-05-05 13:16:30 -0500

    encapsulation

--- src/Encapsulation/encapsulation.java
+++ src/Encapsulation/encapsulation.java
@@ -2,19 +2,42 @@
 //
 //public class encapsulation {
 //    public static void main(String[] args) {
-//        // Encapsulation = attributes of a class will be hidden or private, can only be accessed through methods(getters and setters)
-//        //                 you should make attributes private if you don't have a reason to make them public/protected
+////        // Encapsulation = attributes of a class will be hidden or private, can only be accessed through methods(getters and setters)
+////        //                 you should make attributes private if you don't have a reason to make them public/protected
+////
+////        Car car = new Car("Chevy", "Camaro", 2021);
+////
+////        System.out.println(car.getMake()); // getter method
+////        System.out.println(car.getModel());
+////        System.out.println(car.getYear());
+////
+////        car.setYear(2023); // setter method
+////        System.out.println(car.getMake());
+////        System.out.println(car.getModel());
+////        System.out.println(car.getYear());
 //
-//        Car car = new Car("Chevy", "Camaro", 2021);
+//    /////////////////////////////////////// COPYING OBJECTS ///////////////////////////////////////////////////////////
+//        Car car1 = new Car("Nissan", "GTR", 2020);
 //
-//        System.out.println(car.getMake()); // getter method
-//        System.out.println(car.getModel());
-//        System.out.println(car.getYear());
+//        Car car2 = new Car("Toyota", "Supra", 1995);
+//        car2.copy(car1); // using method made in Car class
 //
-//        car.setYear(2023); // setter method
-//        System.out.println(car.getMake());
-//        System.out.println(car.getModel());
-//        System.out.println(car.getYear());
+//        Car car3 = new Car(car1);
+//
+//        System.out.println(car1);
+//        System.out.println(car2);
+//        System.out.println();
+//        System.out.println(car1.getMake());
+//        System.out.println(car1.getModel());
+//        System.out.println(car1.getYear());
+//        System.out.println();
+//        System.out.println(car2.getMake());
+//        System.out.println(car2.getModel());
+//        System.out.println(car2.getYear());
+//        System.out.println();
+//        System.out.println(car3.getMake());
+//        System.out.println(car3.getModel());
+//        System.out.println(car3.getYear());
 //
 //    }
 //}
