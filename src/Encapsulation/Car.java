c783e1ccf5209d72a6a5c0769ecbfd0ce77db89a
Jared Kim <jaredkim01@yahoo.com>
2023-05-05 12:06:33 -0500

    encapsulation

---
+++ src/Encapsulation/Car.java
@@ -0,0 +1,37 @@
+//package Encapsulation;
+//
+//public class Car {
+//    private String make;  // since these are all private, we make the public getter methods at the bottom to get these private items
+//    private String model;
+//    private int year;
+//
+//    Car(String make, String model, int year) {
+//        this.setMake(make);
+//        this.setModel(model);  // for setters
+//        this.setYear(year);
+//    }
+//
+//    public String getMake() {  //
+//        return make;
+//    }
+//
+//    public String getModel() {  // Getter methods
+//        return model;
+//    }
+//
+//    public int getYear() {
+//        return year;
+//    }                           //
+//
+//    public void setMake(String make) {  //
+//        this.make = make;
+//    }
+//
+//    public void setModel(String model) {  // Setter methods
+//        this.model = model;
+//    }
+//
+//    public void setYear(int year) {
+//        this.year = year;
+//    }                                   //
+//}
---
+++ src/Encapsulation/encapsulation.java
@@ -0,0 +1,20 @@
+//package Encapsulation;
+//
+//public class encapsulation {
+//    public static void main(String[] args) {
+//        // Encapsulation = attributes of a class will be hidden or private, can only be accessed through methods(getters and setters)
+//        //                 you should make attributes private if you don't have a reason to make them public/protected
+//
+//        Car car = new Car("Chevy", "Camaro", 2021);
+//
+//        System.out.println(car.getMake()); // getter method
+//        System.out.println(car.getModel());
+//        System.out.println(car.getYear());
+//
+//        car.setYear(2023); // setter method
+//        System.out.println(car.getMake());
+//        System.out.println(car.getModel());
+//        System.out.println(car.getYear());
+//
+//    }
+//}
