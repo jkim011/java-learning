5afc6682954eb02c36e399fb94bbc5a74c791976
Jared Kim <jaredkim01@yahoo.com>
2023-05-07 23:48:13 -0500

    starting intercaces

---
+++ src/Interface/interfaces.java
@@ -0,0 +1,12 @@
+package Interface;
+
+public class interfaces {
+    public static void main(String[] args) {
+        // interface = a template that can be applied to a class
+        //             similar to inheritance, but specifies what a class has or must do
+        //             classes can apply more than one interface, inheritance is limited to one super
+
+
+
+    }
+}
