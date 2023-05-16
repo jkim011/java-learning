b29f82f5fef983e78ce20043a1faf22966020a40
Jared Kim <jaredkim01@yahoo.com>
2023-05-16 16:44:02 -0500

    file

---
+++ src/FileClass/fileclass.java
@@ -0,0 +1,22 @@
+//package FileClass;
+//
+//import java.io.File;
+//public class fileclass {
+//    public static void main(String[] args) {
+//        // file = an abstract representation of file and directory pathnames
+//
+//        File file = new File("C:/Users/Admin/OneDrive/Desktop/secret_message.TXT");
+//
+//        if(file.exists()) {
+//            System.out.println("That file exists! :O!");
+//            System.out.println(file.getPath());
+//            System.out.println(file.getAbsolutePath());
+//            System.out.println(file.isFile());
+//            // file.delete();  // will delete the secret_message file. have to refresh java class to see it's gone
+//        }
+//        else {
+//            System.out.println("That file doesn't exist :(");
+//        }
+//
+//    }
+//}
