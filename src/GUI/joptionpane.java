adab533218139a4a5fc6d559d6d712b5100cfb5f
Jared Kim <jaredkim01@yahoo.com>
2023-05-28 17:31:53 -0500

    JOptionPane

---
+++ src/GUI/joptionpane.java
@@ -0,0 +1,32 @@
+//package GUI;
+//
+//import javax.swing.*;
+//
+//public class joptionpane {
+//    public static void main(String[] args) {
+//        // JOptionPane = pop up a standard dialog box that prompts users for a value or informs them of something
+//
+//        //JOptionPane.showMessageDialog(null, "This is some info", "Title", JOptionPane.PLAIN_MESSAGE);           //
+//        //JOptionPane.showMessageDialog(null, "Here is some more info", "Title", JOptionPane.INFORMATION_MESSAGE); //
+//        //JOptionPane.showMessageDialog(null, "Do you want some info?", "Title", JOptionPane.QUESTION_MESSAGE);     // ↑ JOption.whatever is diff options for pane style ↓
+//        //JOptionPane.showMessageDialog(null, "You're computer has a virus", "Title", JOptionPane.WARNING_MESSAGE); //
+//        //JOptionPane.showMessageDialog(null, "ERROR 404", "Title", JOptionPane.ERROR_MESSAGE);                    //
+//
+////        int answer = JOptionPane.showConfirmDialog(null, "Do you even code?", "Another Title", JOptionPane.YES_NO_CANCEL_OPTION);
+////        String name = JOptionPane.showInputDialog("What is your name?: ");
+////        System.out.println("Hello " + name);
+//
+//        String[] responses = {"No, you're awesome", "Thanks", "*Blush"};
+//
+//        JOptionPane.showOptionDialog(
+//                null,
+//                "You are awesome",
+//                "Secret message",
+//                JOptionPane.YES_NO_CANCEL_OPTION,
+//                JOptionPane.INFORMATION_MESSAGE,
+//                null,
+//                responses,
+//                0);
+//
+//    }
+//}
