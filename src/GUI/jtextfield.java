66d0e3f0608fba9f25291c8ea647ac4ba847cdba
Jared Kim <jaredkim01@yahoo.com>
2023-05-28 17:47:27 -0500

    JTextField

---
+++ src/GUI/jtextfield.java
@@ -0,0 +1,10 @@
+//package GUI;
+//
+//public class jtextfield {
+//    public static void main(String[] args) {
+//        // JTextField = a GUI textbox component that can be used to add, set, or get text
+//
+//        jtextfieldMyFrame frame = new jtextfieldMyFrame();
+//
+//    }
+//}
