62621a41b93bcf453db2fda74ea337486f3cbb29
Jared Kim <jaredkim01@yahoo.com>
2023-06-05 12:27:24 -0500

    checkboxes

---
+++ src/jtextbox/textbox.java
@@ -0,0 +1,10 @@
+//package jtextbox;
+//
+//public class textbox {
+//    public static void main(String[] args) {
+//        // JCheckBox = a GUI component that can be selected or deselected
+//
+//        new MyFrame();
+//
+//;    }
+//}
